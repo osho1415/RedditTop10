238afcfc0673b168ea880a4a52cbe7e02d8d8c5a
Osho Sharma <71971917+osho1415@users.noreply.github.com>
2023-03-09 04:02:29 -0700

    Add files via upload

---
+++ reddit_scarper.py
@@ -0,0 +1,19 @@
+from bs4 import BeautifulSoup
+import requests
+import csv 
+
+scrape_page = requests.get("https://www.reddit.com/top/")
+soup = BeautifulSoup(scrape_page.text, "html.parser")
+
+write_file = open("top_reddit.csv", "w")
+writer = csv.writer(write_file)
+
+writer.writerow(["TITLE", "URL"])
+posts = soup.find_all("h3", attrs = {"class": "_eYtD2XCVieq6emjKBH3m"},limit=10)
+links = soup.find_all("a", attrs = {"class": "SQnoC3ObvgnGjWt90zD9Z _2INHSNB8V5eaWp4P0rY_mE", "data-click-id":"body"}, limit = 10)
+
+for post,link in zip(posts,links):
+    writer.writerow([post.text, "https://www.reddit.com/" + link['href']])
+
+write_file.close()
+
